c4ee44d6a5765868b35c39a2756163ee462f7b46
romgharti <romgharti@gmail.com>
2025-11-26 13:37:44 +0000

    onyx: Adjust cpu frequency change rate limit
    Signed-off-by: romgharti <romgharti@gmail.com>

--- rootdir/bin/init.kernel.post_boot-tuna_default_2_3_2_1.sh
+++ rootdir/bin/init.kernel.post_boot-tuna_default_2_3_2_1.sh
@@ -151,14 +151,14 @@ if [ -d /proc/sys/walt ]; then
 	echo "walt" > /sys/devices/system/cpu/cpufreq/policy5/scaling_governor
 	echo "walt" > /sys/devices/system/cpu/cpufreq/policy7/scaling_governor
 
-	echo 0 > /sys/devices/system/cpu/cpufreq/policy0/walt/down_rate_limit_us
-	echo 0 > /sys/devices/system/cpu/cpufreq/policy0/walt/up_rate_limit_us
-	echo 0 > /sys/devices/system/cpu/cpufreq/policy2/walt/down_rate_limit_us
-	echo 0 > /sys/devices/system/cpu/cpufreq/policy2/walt/up_rate_limit_us
-	echo 0 > /sys/devices/system/cpu/cpufreq/policy5/walt/down_rate_limit_us
-	echo 0 > /sys/devices/system/cpu/cpufreq/policy5/walt/up_rate_limit_us
-	echo 0 > /sys/devices/system/cpu/cpufreq/policy7/walt/down_rate_limit_us
-	echo 0 > /sys/devices/system/cpu/cpufreq/policy7/walt/up_rate_limit_us
+	echo 1500 > /sys/devices/system/cpu/cpufreq/policy0/walt/down_rate_limit_us
+	echo 750 > /sys/devices/system/cpu/cpufreq/policy0/walt/up_rate_limit_us
+	echo 800 > /sys/devices/system/cpu/cpufreq/policy2/walt/down_rate_limit_us
+	echo 400 > /sys/devices/system/cpu/cpufreq/policy2/walt/up_rate_limit_us
+	echo 500 > /sys/devices/system/cpu/cpufreq/policy5/walt/down_rate_limit_us
+	echo 200 > /sys/devices/system/cpu/cpufreq/policy5/walt/up_rate_limit_us
+	echo 250 > /sys/devices/system/cpu/cpufreq/policy7/walt/down_rate_limit_us
+	echo 100 > /sys/devices/system/cpu/cpufreq/policy7/walt/up_rate_limit_us
 
 	echo 0 > /sys/devices/system/cpu/cpufreq/policy0/walt/pl
 	echo 0 > /sys/devices/system/cpu/cpufreq/policy2/walt/pl
