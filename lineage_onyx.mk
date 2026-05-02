9bdf875c3c356a6866a64137886c5502f97a8c86
Sachinpawar86 <sachinbarange86@gmail.com>
2026-05-02 16:32:24 +0530

    onyx: Enable Reality Engine support in Lunaris build flags Signed-off-by: Sachin <sachinbarange86@gmail.com>

--- lineage_onyx.mk
+++ lineage_onyx.mk
@@ -16,6 +16,7 @@ $(call inherit-product, device/xiaomi/onyx/device.mk)
 # Lunaris Build Flags
 LUNARIS_BUILD_TYPE := OFFICIAL
 WITH_BCR := true
+USE_REALITY_ENGINE := true
 TARGET_CUSTOM_UDFPS := true
 TARGET_OPTIMIZED_DEXOPT := true
 TARGET_BOOT_ANIMATION_RES := 1080
